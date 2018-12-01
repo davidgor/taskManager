90aa7502d775bd16c3d98e501bffc902b9048361
David Gorrebeeck <davidgor55@gmail.com>
2018-12-01 12:50:40 +0100

    creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ManagerComponent } from './manager/manager.component';
+import { UsersComponent } from './users/users.component';
+import { ProcessesComponent } from './processes/processes.component';
+import { StatusComponent } from './status/status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ManagerComponent,
+    UsersComponent,
+    ProcessesComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
